543ee03abf4ca71b327538151165812fbd852de6
Ilya547 <Ilya547@bk.ru>
2023-01-10 15:12:28 +0300

    BJ\HW_15\downloaded HW

--- src/webapp/web/New.java
+++ src/webapp/web/New.java
@@ -6,17 +6,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class ResumeServlet extends HttpServlet {
+public class New extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-////        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-        response.getWriter().write("YES !!!!");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> YES!! </h1>");
+        pw.println("</html>");
+
+
     }
 
     @Override
